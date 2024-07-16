47b574e24c60145a737f54fb6f81e39e5d4761ba
hrmrakib <hrmrakibs@gmail.com>
2024-07-16 11:36:32 +0600

    basic route set-up

--- src/contexts/AuthContextProvider.jsx
+++ src/contexts/AuthContextProvider.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { createContext, useState } from "react";
+import useAxiosPublic from "../hooks/useAxiosPublic";
+
+export const AuthContext = createContext(null);
 
 const AuthContextProvider = () => {
-  return <div>AuthContextProvider</div>;
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const axiosPublic = useAxiosPublic();
+
+  const createUser = () => {};
+
+  return <div></div>;
 };
 
 export default AuthContextProvider;
